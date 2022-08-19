eac06340d6653644dfa0f8983898233c4c373f01
Israel Ponciano <israelponciano22@gmail.com>
2022-08-19 17:18:59 -0300

    Exercícios 7 - 9

---
+++ fundamentos/bloco-4/dia-2/exercicio8.js
@@ -0,0 +1,10 @@
+let array = [];
+
+for (let i = 0; i <= 24;  i += 1) {
+    for (let j = 1; j <= 25;  j += 1) {
+        array.length[i] = array.push(j);
+    }
+    break;
+}
+
+console.log(array);

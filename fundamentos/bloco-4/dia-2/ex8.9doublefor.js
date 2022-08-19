39749322f8be07a1c2af7ccb1aa324b388f1f1e2
Israel Ponciano <israelponciano22@gmail.com>
2022-08-19 17:37:08 -0300

    Exercícios 8 e 9 Corrigidos

--- fundamentos/bloco-4/dia-2/ex8.9doublefor.js
+++ fundamentos/bloco-4/dia-2/ex8.9doublefor.js
@@ -1,10 +1,12 @@
 let array = [];
 
 for (let i = 0; i <= 24;  i += 1) {
-    array.length[i] = array.push(i + 1);
+    for (let j = 1; j <= 25;  j += 1) {
+        array.length[i] = array.push(j);
+    }
+    break;
 }
 
 for (let i = 0; i < array.length ;  i += 1) {
     console.log(array[i] / 2);
 }
-    
--- fundamentos/bloco-4/dia-2/exercicio8.js
+++ fundamentos/bloco-4/dia-2/exercicio8.js
@@ -1,10 +1,7 @@
 let array = [];
 
 for (let i = 0; i <= 24;  i += 1) {
-    for (let j = 1; j <= 25;  j += 1) {
-        array.length[i] = array.push(j);
+    array.length[i] = array.push(i + 1);
     }
-    break;
-}
 
 console.log(array);

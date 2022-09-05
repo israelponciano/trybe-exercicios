ffe3da4831932e34fdfd7e8cc0f5305050d5ed49
Israel Ponciano <israelponciano22@gmail.com>
2022-09-05 19:11:26 -0300

    dia 5.3 atrasado/ partiu projeto

--- fundamentos/bloco-5/dia-3/main.js
+++ fundamentos/bloco-5/dia-3/main.js
@@ -9,17 +9,61 @@ const myWebpage = document.getElementById('my-spotrybefy');
 // 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
+firstLi.addEventListener('click', techClickFirst);
+secondLi.addEventListener('click', techClickSecond);
+thirdLi.addEventListener('click', techClickThird);
+function techClickFirst() {
+  firstLi.className = 'tech';
+  secondLi.className = '';
+  thirdLi.className = '';
+}
+function techClickSecond() {
+  secondLi.className = 'tech';
+  firstLi.className = '';
+  thirdLi.className = '';
+}
+function techClickThird() {
+  firstLi.className = '';
+  secondLi.className = '';
+  thirdLi.className = 'tech';
+}
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
 
-// 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
-// com a classe 'tech';
+// 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento com a classe 'tech';
+input.addEventListener('change', techTexto);
+function techTexto() {
+  if (firstLi.className == 'tech'){
+    let valorInput = input.value;
+    firstLi.innerText = valorInput;
+  }
+  if (secondLi.className == 'tech'){
+    let valorInput = input.value;
+    secondLi.innerText = valorInput;
+  }
+  if (thirdLi.className == 'tech'){
+    let valorInput = input.value;
+    thirdLi.innerText = valorInput;
+  }
+}
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
+myWebpage.addEventListener('dblclick', dbClickTitulo);
+function dbClickTitulo() {
+  window.open('https://www.google.com/');
+}
 // 4.1. Que tal redirecionar para seu portfólio?
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+myWebpage.addEventListener('mouseover', mouseEmCima);
+myWebpage.addEventListener('mouseleave', mouseSaiu);
+function mouseEmCima() {
+  myWebpage.style.color = 'red';
+}
+function mouseSaiu() {
+  myWebpage.style.color = 'white';
+}
 
 // Segue abaixo um exemplo do uso de event.target:
 
